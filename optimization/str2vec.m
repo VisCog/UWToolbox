fbcae57350f5dc652d66150775150cf32bd94549
Kelly Chang <kchang4@uw.edu>
2017-11-26 21:00:11 -0800

    edited for : only notation

--- optimization/str2vec.m
+++ optimization/str2vec.m
@@ -1,11 +1,11 @@
 function [indx] = str2vec(param, str)
 % [indx] = str2vec(param, str)
 %
-% Extracts linearized 'indx' indices from 'param' parameter corresponding 
-% with 'str'. Support function to 'params2var.m', 'params2varcon.m', and 
-% 'var2params.m'. 
+% Extracts linearized 'indx' indices from 'param' parameter corresponding
+% with 'str'. Support function to 'params2var.m', 'params2varcon.m', and
+% 'var2params.m'.
 %
-% Inputs: 
+% Inputs:
 %   param       The parameter that has values that are to be fitted
 %
 %   str         A string of the values within 'params' that to be fitted
@@ -21,9 +21,18 @@ function [indx] = str2vec(param, str)
 
 %% Linearize Index for Value(s) to be Fitted in 'param'
 
+str = '(:,:,1)';
+
 if isempty(str)
     indx = 1:numel(param);
 else
+    if ~isempty(regexp(str, '(\(|,):', 'once'))
+        dims = discretize(regexp(str, '(\(|,):'), [0 regexp(str, ',') length(str)]);
+        for i = 1:length(dims)
+            indx = regexp(str, '(\(|,):', 'once');
+            str = [str(1:indx) sprintf('1:size(param,%d)',dims(i)) str((indx+2):end)];
+        end
+    end
     indx = eval(sprintf('CombVec%s;', str));
     nIndx = arrayfun(@(x) sprintf('indx(%d,:)',x), 1:size(indx,1), 'UniformOutput', false);
     indx = eval(sprintf('sub2ind(size(param),%s);', strjoin(nIndx, ',')));
