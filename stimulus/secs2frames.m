cc87c34090147dd13be00627a43c04c913ed83bc
Kelly Chang <kchang4@uw.edu>
2017-11-29 00:37:56 -0800

    minor documentation editing

--- stimulus/secs2frames.m
+++ stimulus/secs2frames.m
@@ -3,19 +3,23 @@ function [frames] = secs2frames(display, seconds)
 %
 % Converts time in seconds to frames by calling:
 %
-% frames = round(seconds * display.frameRate)
+% frames = ceil(seconds * display.frameRate)
 % 
 % Inputs:
 %   display           A structure containing display information (see 
-%                     OpenWidow.m), must have field:
-%       frameRate     Frame rate in Hz, as determined by
+%                     OpenWindow.m), must have field:
+%       fr            Frame rate in Hz, as determined by
 %                     Screen('GetFlipInterval'), numeric
 % 
 %   seconds           Time in seconds to be converted to frames, seconds
+%
+% Ouput: 
+%   frames            The translated number of frames for the given display
+%                     information and seconds, numeric
 
 % Written by G.M. Boynton - 11/16/07 
 % Edited by Kelly Chang - February 23, 2017
 
 %% Convert Seconds to Frames
 
-frames = round(seconds * display.frameRate);
+frames = ceil(seconds * display.fr);
--- stimulus/wait4key.m
+++ stimulus/wait4key.m
@@ -1,14 +1,16 @@
 function [keyPressed] = wait4key(keys, deviceNum)
 % [keyPressed] = wait4key(keys, deviceNum)
 %
-% Waits for the any specified key to be pressed.
+% Waits for the any specified 'keys' to be pressed.
 %
 % Inputs:
 %   keys             Keys to wait for, string or cell array of strings
 %                    (default: 't')
 %
 %   deviceNum        Device number to wait for the specified key(s) press
-%                    (default: -3)
+%                    (default: -3,  for all keyboard devices, see KBCHECK.m 
+%                    for additional information about keyboard querying 
+%                    numbers)
 %
 % Output:
 %   keyPressed       Which key was pressed, string
@@ -22,6 +24,10 @@ function [keyPressed] = wait4key(keys, deviceNum)
 
 if ~exist('keys', 'var')
     keys = {'t'};
+end
+
+if ischar(keys)
+    keys = {keys};
 end
 
 if ~exist('deviceNum', 'var')
@@ -40,13 +46,10 @@ if ~exist('keys', 'var') % if no key(s) specified, wait for anything
     while ~any(keyCode)
         [~,~,keyCode] = KbCheck(deviceNum);
     end
-else % if any arg is specified, key is specified
-    if ischar(keys)
-        keys = {keys};
-    end
+else % if key is specified
     [~,~,keyCode] = KbCheck(deviceNum);
     while ~any(strcmp(KbName(keyCode), keys))
         [~,~,keyCode] = KbCheck(deviceNum);
     end
 end
-keyPressed = KbName(keyCode); % what key was pressed
+keyPressed = KbName(keyCode); % return  which key was pressed
