d21df045881a788bdbf0f42fb1b1286fb295a8e9
Kelly Chang <kchang4@uw.edu>
2017-11-28 23:07:52 -0800

    initial timeLeftBar.m

--- misc/timeLeftBar.m
+++ misc/timeLeftBar.m
@@ -1,9 +1,16 @@
-function timeLeftBar(rep, n, message)
-% TIMELEFTBAR Display time based wait bar
-% timeLeftBar(rep, n, message) creates and displays
-% a variant of waitbar that is in real time
-% rep should be the rep value in the loop
-% n should be the maximum value for the loop
+function timeLeftBar(i, n, msg)
+% timeLeftBar(i, n, msg)
+% 
+% Creates and displays a real time based wait bar (instead of progress 
+% based).
+% 
+% Inputs: 
+%   i            The iterated value the loop is currently on, numeric
+%
+%   n            The maximum value the loop will go through, numeric
+%
+%   msg          The message the wait bar displays, string 
+%                (default: 'Time Left:')
 %
 % Example:
 % n = 1000;
@@ -14,39 +21,37 @@ function timeLeftBar(rep, n, message)
 % end
 
 % Written by Geoffrey M. Boynton. Edited Ione Fine.
-% Edited by Kelly Chang - March 16, 2017
+% Edited by Kelly Chang - November 28, 2017
 
 %% Input Control
 
 h = findobj(allchild(0), 'flat', 'Tag', 'TMWWaitbar');
 if isempty(h)
     h = waitbar(1); % initialize waitbar if none
-    tic;
+    tic; % start time
 end
 
-if ~exist('message', 'var')
-    message = 'Time left:';
+if ~exist('msg', 'var')
+    msg = 'Time Left:';
 end
 
-if rep > n
-    error('timeLeftBar: rep number > maximum number');
+if i > n
+    error('timeLeftBar: i number > maximum number');
 end
 
 %% Calculate and Display Time Left
 
-timeLeft = toc*(n-rep)/rep; % seconds
+timeLeft = toc*(n-i)/i; % seconds
 if timeLeft < 60 % less than a minute
     timeLeftStr = sprintf('%d s', floor(timeLeft));
 elseif timeLeft < (60*60)  % between one minute and an hour
-    timeLeftStr = sprintf('%d min %d s',floor(timeLeft/60), floor(mod(timeLeft,60)));
+    timeLeftStr = sprintf('%d min %d s', floor(timeLeft/60), floor(mod(timeLeft,60)));
 else % more than an hour (show hours and minutes)
     timeLeftStr = sprintf('%d hr %d min', floor(timeLeft/3600), ...
         floor(floor(mod(timeLeft,3600)/60)));
 end
-waitbar(rep/n, h, sprintf('%s %s', message, timeLeftStr));
+waitbar(i/n, h, sprintf('%s %s', msg, timeLeftStr)); % update wait bar
 
-%% Clear Wait Bar Handle
-
-if rep == n
+if i == n  % clean up wait bar
     delete(h);
 end
