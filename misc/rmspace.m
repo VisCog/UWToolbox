ea79889ff294b8ac20d4ad850bede99e7bc6cc06
Kelly Chang <kchang4@uw.edu>
2017-11-28 21:40:10 -0800

    cleaned and renamed rmspace.m

---
+++ misc/rmspace.m
@@ -0,0 +1,28 @@
+function [out] = rmspace(str)
+% [out] = rmspace(str)
+% 
+% Removes spaces in a string or a cell array.
+%
+% Input: 
+%   str          A string or cell array of strings
+%
+% Output: 
+%   out          String or cell array with spaces removed.   
+% 
+% Example:
+% out = rmspace({'4 6  34 cat'})
+
+% Written Geoffrey M. Boynton
+% Edited Ione Fine 2/24/2017
+% Edited Kelly Chang - November 27, 2017
+
+%% Remove Spaces
+
+switch class(str)
+    case 'char'
+        out = regexprep(str,'[\s]','');
+    case 'cell'
+        out = cellfun(@(x) regexprep(x,'[\s]',''), str, 'UniformOutput', false);
+    otherwise
+        error('Unrecognized data class: %s\n', class(str));
+end
